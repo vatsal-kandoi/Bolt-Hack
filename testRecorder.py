ee4d563e25df3e8557feb241ac987fa935a1bdce
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-02-25 01:08:08 +0530

    Minor change

--- testRecorder.py
+++ testRecorder.py
@@ -3,7 +3,7 @@ import time
 
 rec = Recorder(channels=2) 
 print(rec.get_device_info())  
-recfile = self.rec.open('a.wav', 'wb')    
+recfile = rec.open('a.wav', 'wb')    
 recfile.start_recording();
 time.sleep(2);
 recfile.stop_recording();

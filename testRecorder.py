55af404db8a3b2f6ca99a3e0c186eb8199e9aff5
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-02-24 20:33:29 +0000

    Added minor files

--- testRecorder.py
+++ testRecorder.py
@@ -1,8 +1,7 @@
 from recorder import Recorder
 import time
 
-rec = Recorder(channels=2) 
-print(rec.get_device_info())  
+rec = Recorder(channels=2)  
 recfile = rec.open('a.wav', 'wb')    
 recfile.start_recording();
 time.sleep(2);
